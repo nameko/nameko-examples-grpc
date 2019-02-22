20ed332187109d7297274957fff17a040d18ad41
Jakub Borys <jakub.borys@gmail.com>
2019-02-22 14:43:23 +0000

    Orders schema

--- gateway/src/schema.ts
+++ gateway/src/schema.ts
@@ -9,17 +9,31 @@ const typeDefs = gql`
     inStock: Int
   }
 
+  type OrderDetail {
+    id: ID!
+    product: Product!
+    price: String
+    quantity: Int
+  }
+
+  type Order {
+    id: ID!
+    orderDetails: [OrderDetail]!
+  }
+
+  type Query {
+    products: [Product]
+    product(id: ID!): Product
+    order(id: ID!): Order
+    orders: [Order]
+  }
+
   input ProductInput {
     id: ID!
     title: String
     passengerCapacity: Int
     maximumSpeed: Int
     inStock: Int
-  }
-
-  type Query {
-    products: [Product]!
-    product(id: ID!): Product
   }
 
   type Mutation {
